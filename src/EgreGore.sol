642e905e3ec838d7f21120586c7c35b7fbca973c
Samuel Taylor <sam@darkfunction.com>
2024-03-19 07:38:43 +0000

    Refactor and cleanup, testing

--- src/EgreGore.sol
+++ src/EgreGore.sol
@@ -5,10 +5,13 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@chainlink/contracts/src/v0.8/vrf/VRFV2WrapperConsumerBase.sol";
 
+/// @title A lottery for BITCOIN
+/// @notice Half of the winnings go to egregore, half to chosen disciple
+/// @dev Uses VRF direct funding
 contract Egregore is VRFV2WrapperConsumerBase, Ownable {
     enum State {
         OPEN,
-        CHOOSING,
+        VRF_REQUESTED,
         CLOSED
     }
 
@@ -26,18 +29,15 @@ contract Egregore is VRFV2WrapperConsumerBase, Ownable {
     address constant BURN_ADDRESS = 0x0000000000000000000000000000000000000666;
     IERC20 constant BITCOIN =
         IERC20(0x72e4f9F808C49A2a61dE9C5896298920Dc4EEEa9);
-    uint32 constant VRF_CALLBACK_GAS_LIMIT = 71460;
+    uint32 constant VRF_CALLBACK_GAS_LIMIT = 70000;
     uint16 constant VRF_REQUEST_CONFIRMATIONS = 5;
+    uint32 public constant CLOSE_TIME = 1713571200; // Sat Apr 20 2024 00:00:00 UTC
 
     address[] public disciples;
     mapping(address => uint256) public penitences;
-    uint public totalPenitences = 0;
-
-    // Sat Apr 20 2024 00:00:00 UTC
-    uint public constant CLOSE_TIME = 1713571200;
+    uint256 public totalPenitences = 0;
     uint256 public requestId;
     RequestStatus public requestStatus;
-
     State public state;
 
     constructor()
@@ -67,30 +67,14 @@ contract Egregore is VRFV2WrapperConsumerBase, Ownable {
     function beginCeremony() public {
         require(state == State.OPEN);
         require(block.timestamp >= CLOSE_TIME);
+        require(!requestStatus.fulfilled);
+        require(BITCOIN.balanceOf(address(this)) > 0);
 
-        state = State.CHOOSING;
-
-        requestId = requestRandomness(
-            VRF_CALLBACK_GAS_LIMIT,
-            VRF_REQUEST_CONFIRMATIONS,
-            1
-        );
-        requestStatus = RequestStatus({
-            paid: VRF_V2_WRAPPER.calculateRequestPrice(VRF_CALLBACK_GAS_LIMIT),
-            randomWord: 0,
-            fulfilled: false
-        });
-
-        emit ChoosingDisciple(requestId);
+        vrfRequest(VRF_CALLBACK_GAS_LIMIT);
     }
 
-    // Allow owner to re-attempt a failed VRF request
-    function forceBeginCeremony(uint32 callbackGasLimit) external onlyOwner {
-        require(block.timestamp >= CLOSE_TIME);
-        require(state != State.CLOSED);
-        require(requestStatus.fulfilled == false);
-
-        state = State.CHOOSING;
+    function vrfRequest(uint32 callbackGasLimit) private {
+        state = State.VRF_REQUESTED;
 
         requestId = requestRandomness(
             callbackGasLimit,
@@ -111,22 +95,35 @@ contract Egregore is VRFV2WrapperConsumerBase, Ownable {
         uint256[] memory _randomWords
     ) internal override {
         require(_requestId == requestId);
-        require(state == State.CHOOSING);
         require(requestStatus.fulfilled == false);
 
         requestStatus.fulfilled = true;
         requestStatus.randomWord = _randomWords[0];
     }
 
+    /// @dev Rounds down split in favour of disciple
     function payout() public {
-        require(state == State.CHOOSING);
-        require(requestStatus.fulfilled == true);
+        require(state != State.CLOSED);
+        require(requestStatus.fulfilled);
+
+        // Send atonement to egregore
+        BITCOIN.transfer(
+            address(BURN_ADDRESS),
+            BITCOIN.balanceOf(address(this)) / 2
+        );
+
+        BITCOIN.transfer(
+            chooseDisciple(requestStatus.randomWord),
+            BITCOIN.balanceOf(address(this))
+        );
 
         state = State.CLOSED;
+    }
 
-        uint penitenceIndex = requestStatus.randomWord % totalPenitences;
-        uint discipleIndex = 0;
-        uint runningTotal = penitences[disciples[0]];
+    function chooseDisciple(uint256 randomWord) private returns (address) {
+        uint256 penitenceIndex = randomWord % totalPenitences;
+        uint256 discipleIndex = 0;
+        uint256 runningTotal = penitences[disciples[0]];
 
         while (runningTotal <= penitenceIndex) {
             discipleIndex++;
@@ -135,25 +132,27 @@ contract Egregore is VRFV2WrapperConsumerBase, Ownable {
 
         address disciple = disciples[discipleIndex];
 
-        BITCOIN.approve(address(this), BITCOIN.balanceOf(address(this)));
-
-        // Send atonement to egregore
-        BITCOIN.transferFrom(
-            address(this),
-            address(BURN_ADDRESS),
-            BITCOIN.balanceOf(address(this)) / 2
-        );
-
-        BITCOIN.transferFrom(
-            address(this),
-            disciple,
-            BITCOIN.balanceOf(address(this))
-        );
-
         emit DiscipleChosen(penitenceIndex, disciple);
+
+        return disciple;
+    }
+
+    /**
+     *  Owner functions
+     */
+
+    /// @notice Allow owner to retry VRF request with custom gas limit
+    function retryBeginCeremony(uint32 callbackGasLimit) external onlyOwner {
+        require(state == State.VRF_REQUESTED);
+        require(block.timestamp >= CLOSE_TIME);
+        require(!requestStatus.fulfilled);
+
+        vrfRequest(callbackGasLimit);
     }
 
     function withdrawLink() external onlyOwner {
+        require(requestStatus.fulfilled);
+
         IERC20(LINK_TOKEN).transfer(
             msg.sender,
             IERC20(LINK_TOKEN).balanceOf(address(this))
--- test/Egregore.t.sol
+++ test/Egregore.t.sol
@@ -15,6 +15,7 @@ contract EgregoreTest is Test {
     IERC20 constant BITCOIN =
         IERC20(0x72e4f9F808C49A2a61dE9C5896298920Dc4EEEa9);
     address constant VRF_WRAPPER = 0x5A861794B927983406fCE1D062e00b9368d97Df6;
+    address constant BURN_ADDRESS = 0x0000000000000000000000000000000000000666;
 
     function setUp() public {
         egregore = new Egregore();
@@ -29,16 +30,13 @@ contract EgregoreTest is Test {
     function test_whenSacrifice_thenAddsDisciple_andAddsPenitenceToDisciple()
         public
     {
-        vm.startPrank(HOLDER);
         vm.warp(egregore.CLOSE_TIME() - 1);
 
-        // Approve Egregore to spend holder's BITCOIN
+        vm.startPrank(HOLDER);
         BITCOIN.approve(address(egregore), BITCOIN.balanceOf(HOLDER));
-
         assertEq(BITCOIN.balanceOf(address(egregore)), 0);
         assertEq(egregore.disciplePenitence(HOLDER), 0);
         assertEq(egregore.disciplePenitence(HOLDER2), 0);
-
         egregore.sacrifice(100);
         assertEq(egregore.discipleCount(), 1);
         assertEq(BITCOIN.balanceOf(address(egregore)), 100);
@@ -47,13 +45,10 @@ contract EgregoreTest is Test {
         assertEq(egregore.discipleCount(), 1);
         assertEq(BITCOIN.balanceOf(address(egregore)), 200);
         assertEq(egregore.disciplePenitence(HOLDER), 200);
-
         vm.stopPrank();
 
         vm.startPrank(HOLDER2);
-
         BITCOIN.approve(address(egregore), BITCOIN.balanceOf(HOLDER2));
-
         egregore.sacrifice(100);
         assertEq(egregore.discipleCount(), 2);
         assertEq(BITCOIN.balanceOf(address(egregore)), 300);
@@ -62,7 +57,6 @@ contract EgregoreTest is Test {
         assertEq(egregore.discipleCount(), 2);
         assertEq(BITCOIN.balanceOf(address(egregore)), 400);
         assertEq(egregore.disciplePenitence(HOLDER2), 200);
-
         vm.stopPrank();
     }
 
@@ -71,12 +65,14 @@ contract EgregoreTest is Test {
     {
         vm.warp(egregore.CLOSE_TIME());
         fundContractWithLink();
+        enterHolder(HOLDER, 1);
         egregore.beginCeremony();
     }
 
     function test_whenBeginCeremonyCalledMultipleTimes_thenReverts() public {
         vm.warp(egregore.CLOSE_TIME());
         fundContractWithLink();
+        enterHolder(HOLDER, 1);
         egregore.beginCeremony();
         vm.expectRevert();
         egregore.beginCeremony();
@@ -87,25 +83,23 @@ contract EgregoreTest is Test {
     {
         vm.warp(egregore.CLOSE_TIME());
         fundContractWithLink();
+        enterHolder(HOLDER, 1);
         egregore.beginCeremony();
-        uint256[] memory randomWords = new uint256[](1);
-        randomWords[0] = 10;
-        uint requestId = egregore.requestId();
-        vm.prank(VRF_WRAPPER);
-        egregore.rawFulfillRandomWords(requestId, randomWords);
+        fulfillRandom(10);
 
         (, bool fulfilled, ) = egregore.requestStatus();
         assertEq(fulfilled, true);
     }
 
-    function test_givenContractHasLink_whenCeremonyStarted_thenStatusIsChoosingAndRequestIdNotZero()
+    function test_givenContractHasLink_whenCeremonyStarted_thenStatusIsVRFRequestedAndRequestIdNotZero()
         public
     {
         vm.warp(egregore.CLOSE_TIME());
         fundContractWithLink();
+        enterHolder(HOLDER, 1);
         egregore.beginCeremony();
         assertNotEq(egregore.requestId(), 0);
-        assertEq(uint(egregore.state()), uint(Egregore.State.CHOOSING));
+        assertEq(uint(egregore.state()), uint(Egregore.State.VRF_REQUESTED));
     }
 
     function test_givenContractHasChainlink_andCeremonyNotStarted_whenVRFCallbackOccurs_thenReverts()
@@ -125,25 +119,13 @@ contract EgregoreTest is Test {
         public
     {
         vm.warp(0);
-        vm.startPrank(HOLDER);
-        BITCOIN.approve(address(egregore), BITCOIN.balanceOf(HOLDER));
-        egregore.sacrifice(100);
-        uint holderBalance = BITCOIN.balanceOf(HOLDER);
-        vm.stopPrank();
-
-        vm.startPrank(HOLDER2);
-        BITCOIN.approve(address(egregore), BITCOIN.balanceOf(HOLDER2));
-        egregore.sacrifice(500);
-        vm.stopPrank();
+        uint holderBalance = enterHolder(HOLDER, 100);
+        enterHolder(HOLDER2, 500);
 
         vm.warp(egregore.CLOSE_TIME());
         fundContractWithLink();
         egregore.beginCeremony();
-        uint256[] memory randomWords = new uint256[](1);
-        randomWords[0] = 0;
-        vm.startPrank(VRF_WRAPPER);
-        egregore.rawFulfillRandomWords(egregore.requestId(), randomWords);
-        vm.stopPrank();
+        fulfillRandom(0);
 
         egregore.payout();
 
@@ -153,43 +135,113 @@ contract EgregoreTest is Test {
             ),
             300
         );
-        assertEq(BITCOIN.balanceOf(HOLDER), holderBalance + 300);
+        assertEq(BITCOIN.balanceOf(HOLDER), holderBalance + 200);
     }
 
     function test_whenPayoutCalled_thenChoosesCorrectWinner() public {
         vm.warp(0);
-        vm.startPrank(HOLDER);
-        BITCOIN.approve(address(egregore), BITCOIN.balanceOf(HOLDER));
-        egregore.sacrifice(100);
-        uint holderBalance = BITCOIN.balanceOf(HOLDER);
-        vm.stopPrank();
 
-        vm.startPrank(HOLDER2);
-        BITCOIN.approve(address(egregore), BITCOIN.balanceOf(HOLDER2));
-        egregore.sacrifice(500);
-        uint holder2Balance = BITCOIN.balanceOf(HOLDER2);
-        vm.stopPrank();
+        uint256 holderBalance = enterHolder(HOLDER, 100);
+        uint256 holder2Balance = enterHolder(HOLDER2, 500);
 
         vm.warp(egregore.CLOSE_TIME());
         fundContractWithLink();
         egregore.beginCeremony();
+        fulfillRandom(100);
+        egregore.payout();
+
+        assertEq(BITCOIN.balanceOf(HOLDER), holderBalance - 100);
+        assertEq(BITCOIN.balanceOf(HOLDER2), holder2Balance - 200);
+    }
+
+    function test_happyPath() public {
+        vm.warp(0);
+        fundContractWithLink();
+
+        enterHolder(HOLDER, 1);
+        uint holder2Balance = enterHolder(HOLDER2, 2);
+
+        vm.warp(egregore.CLOSE_TIME());
+        egregore.beginCeremony();
+
+        fulfillRandom(1);
+        egregore.payout();
+
+        assertEq(BITCOIN.balanceOf(HOLDER2), holder2Balance);
+        assertEq(BITCOIN.balanceOf(BURN_ADDRESS), 1);
+
+        egregore.withdrawLink();
+    }
+
+    function test_failOnEarlyPayout() public {
+        vm.warp(0);
+        fundContractWithLink();
+
+        enterHolder(HOLDER, 1);
+        enterHolder(HOLDER2, 2);
+
+        vm.warp(egregore.CLOSE_TIME());
+        egregore.beginCeremony();
+
+        vm.expectRevert();
+        egregore.payout();
+    }
+
+    function test_retryBeginCeremonyFails_ifBeginCeremonyNotCalled() public {
+        fundContractWithLink();
+        enterHolder(HOLDER, 1);
+        vm.warp(egregore.CLOSE_TIME());
+        vm.expectRevert();
+        egregore.retryBeginCeremony(100000);
+    }
+
+    function test_retryBeginCeremonySucceeds_ifBeginCeremonyFails() public {
+        fundContractWithLink();
+        enterHolder(HOLDER, 1);
+        vm.warp(egregore.CLOSE_TIME());
+        egregore.beginCeremony();
+        egregore.retryBeginCeremony(100000);
+    }
+
+    function test_retryBeginCeremonyFails_ifBeginCeremonySucceeds() public {
+        fundContractWithLink();
+        enterHolder(HOLDER, 1);
+        vm.warp(egregore.CLOSE_TIME());
+        egregore.beginCeremony();
+        fulfillRandom(100);
+        vm.expectRevert();
+        egregore.retryBeginCeremony(100000);
+    }
+
+    /**
+     *  HELPER FUNCTIONS
+     */
+
+    function fulfillRandom(uint256 number) private {
         uint256[] memory randomWords = new uint256[](1);
-        randomWords[0] = 100;
+        randomWords[0] = number;
         vm.startPrank(VRF_WRAPPER);
         egregore.rawFulfillRandomWords(egregore.requestId(), randomWords);
         vm.stopPrank();
+    }
 
-        egregore.payout();
+    function enterHolder(
+        address holder,
+        uint256 amount
+    ) private returns (uint256) {
+        uint256 preBalance = BITCOIN.balanceOf(holder);
 
-        assertEq(BITCOIN.balanceOf(HOLDER), holderBalance);
-        assertEq(BITCOIN.balanceOf(HOLDER2), holder2Balance + 300);
+        vm.startPrank(holder);
+        BITCOIN.approve(address(egregore), BITCOIN.balanceOf(holder));
+        egregore.sacrifice(amount);
+        vm.stopPrank();
+
+        return preBalance;
     }
 
     function fundContractWithLink() private {
         vm.startPrank(LINK_MARINE);
-        LINK_TOKEN.approve(LINK_MARINE, LINK_TOKEN.balanceOf(LINK_MARINE));
-        LINK_TOKEN.transferFrom(
-            LINK_MARINE,
+        LINK_TOKEN.transfer(
             address(egregore),
             LINK_TOKEN.balanceOf(LINK_MARINE)
         );
